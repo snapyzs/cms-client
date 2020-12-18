d3e3649f25bbf7d0d57054415c1090871473c5f0
snapyzs <elis_san@mail.ru>
2020-12-18 20:59:58 +0300

    fix

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="content-page center row " :class="{open: isOpen}">
+  <Preloader class="center" v-if="loader"/>
+  <div v-else-if="!loader">
+  <div class="content-page center row "  :class="{open: isOpen}">
     <form class="col s8 m8 l10 center-block">
       <div class="input-field">
         <input id="search" type="search" v-model="store.searchQ" required>
       </div>
     </form>
   </div>
-  <Preloader class="center" v-if="loader"/>
-  <div class="content-page center row" :class="{open: isOpen}" v-else-if="useSearch.reposMatch.value.length">
+  <div class="content-page center row" :class="{open: isOpen}" >
     <div class="content_block z-depth-1" v-for="post in useSearch.reposMatch.value" :key="post._id">
       <ul><img src="" alt=""><a href="#"></a>
         <li>{{post.title}}</li>
@@ -16,9 +17,8 @@
       </ul>
     </div>
   </div>
-  <div class="noFound col s12 m12 l12 center" :class="{open: isOpen}" v-else >
-    <h1>Post not find, for as you request</h1>
   </div>
+
 </template>
 
 <script>
